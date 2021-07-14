c57a90e0558f6a27ccfdd4ff98f50019d31bc3c8
Yujin <nohyujin99@gmail.com>
2021-07-14 20:00:57 +0900

    2021-07-14 연결리스트 과제

---
+++ 7-1.py
@@ -0,0 +1,29 @@
+class Node:
+    def __init__(self, item):
+        self.data = item
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.nodeCount = 0
+        self.head = None
+        self.tail = None
+
+    def getAt(self, pos):
+        if pos < 1 or pos > self.nodeCount:
+            return None
+        i = 1
+        curr = self.head
+        while i < pos:
+            curr = curr.next
+            i += 1
+        return curr
+
+    def traverse(self):
+    	list = []
+        if self.nodeCount == 0:
+            pass
+        else:
+            for idx in range(1,self.nodeCount+1):
+                list.append(self.getAt(idx).data)
+        return list
